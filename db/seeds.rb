241f267c1542c5634982c0f0f3781b26916bf02a
Anikin Artyom <aanikin@naumen.ru>
2017-07-08 03:47:04 +0500

    Remove noise from seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-#
-
+# Create Admin user
 User.create(
   first_name: "Admin",
   last_name: "System",
@@ -15,16 +7,3 @@ User.create(
   password_confirmation: "123",
   role: "admin"
 )
-
-# users_data = lambda do
-#   return {
-#     first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name,
-#     email: Faker::Internet.email,
-#     password: '123',
-#     password_confirmation: '123',
-#     role: 'user'
-#   }
-# end
-
-# User.create([ (0..10).map { users_data.call } ])
