f62f64dd0a76c41f9ccec36e483081b50377bcc1
Anikin Artyom <aanikin@naumen.ru>
2017-07-08 03:45:56 +0500

    Add interface for add and remove user from group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,12 @@
 class Group < ApplicationRecord
-  has_many :groups_users
+  has_many :groups_users, dependent: :destroy
   has_many :users, through: :groups_users
+
+  def add_user(user)
+    users << user
+  end
+
+  def remove_user(user)
+    users.destroy(user)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password
-
-  has_many :groups_users
+  has_many :groups_users, dependent: :destroy
   has_many :groups, through: :groups_users
+
+  has_secure_password
 end
