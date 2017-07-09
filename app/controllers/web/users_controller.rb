b6b2c67de73d6b698a56fc1223d6f963909578c9
Anikin Artyom <aanikin@naumen.ru>
2017-07-09 18:41:57 +0500

    Add reponders for UsersController

--- app/controllers/web/users_controller.rb
+++ app/controllers/web/users_controller.rb
@@ -1,15 +1,10 @@
 class Web::UsersController < Web::ApplicationController
   def new
-    @user = User.new
+    respond_with(@user = User.new)
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_path
-    else
-      render :new
-    end
+    respond_with(@user = User.create(user_params), location: -> { root_path })
   end
 
   private
