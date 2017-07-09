1645ff2566fddbaf3414984daf18f69f4092269f
Anikin Artyom <aanikin@naumen.ru>
2017-07-09 18:39:26 +0500

    Add responders for TestsController

--- app/controllers/web/tests_controller.rb
+++ app/controllers/web/tests_controller.rb
@@ -2,40 +2,30 @@ class Web::TestsController < Web::ApplicationController
   before_action :set_test, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tests = Test.all
+    respond_with(@tests = Test.all)
   end
 
   def show
   end
 
   def new
-    @test = Test.new
+    respond_with(@test = Test.new)
   end
 
   def create
-    @test = Test.new(test_params)
-
-    if @test.save
-      redirect_to tests_path, notice: "Test was successfully created"
-    else
-      render :new
-    end
+    respond_with(@test = Test.create(test_params), location: -> { tests_path })
   end
 
   def edit
   end
 
   def update
-    if @test.update(test_params)
-      redirect_to test_path, notice: "Test was successfully updated"
-    else
-      render :edit
-    end
+    @test.update(test_params)
+    respond_with(@test, location: -> { tests_path })
   end
 
   def destroy
-    @test.destroy
-    redirect_to tests_path, notice: "Test was successfully destroyed"
+    respond_with(@test.destroy, location: -> { tests_path })
   end
 
   private
